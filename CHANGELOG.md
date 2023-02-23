41853b8aaa904187ffc5814841918d251cbb4714
MrSliddes <59771866+MrSliddes@users.noreply.github.com>
2023-02-23 10:51:14 +0100

    v1.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
+## [1.2.1] - 2023-02-23
+### Added
+- IInteraction.GameObject();
+### Changed
+- IInteraction.cs param name gameObject to invoker
+
 ## [1.2.0] - 2023-01-23
 ### Added
 - A bunch of new scripts
--- Runtime/Scripts/Interacting/IInteraction.cs
+++ Runtime/Scripts/Interacting/IInteraction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,8 +21,8 @@ namespace SLIDDES.Interacting
         /// <summary>
         /// When the interaction first gets enterd
         /// </summary>
-        /// <param name="gameObject">The gameObject that wants to interact</param>
-        public virtual void Enter(GameObject gameObject)
+        /// <param name="invoker">The gameObject that wants to interact</param>
+        public virtual void Enter(GameObject invoker)
         {
 
         }
@@ -29,8 +30,8 @@ namespace SLIDDES.Interacting
         /// <summary>
         /// Interact with the gameobject
         /// </summary>
-        /// <param name="gameObject">The gameObject that wants to interact</param>        
-        public virtual void Update(GameObject gameObject)
+        /// <param name="invoker">The gameObject that wants to interact</param>        
+        public virtual void Update(GameObject invoker)
         {
 
         }
@@ -38,10 +39,19 @@ namespace SLIDDES.Interacting
         /// <summary>
         /// Exit an interaction
         /// </summary>
-        /// <param name="gameObject">The gameobject that wants to exit the interaction</param>
-        public virtual void Exit(GameObject gameObject)
+        /// <param name="invoker">The gameobject that wants to exit the interaction</param>
+        public virtual void Exit(GameObject invoker)
         {
 
+        }
+
+        /// <summary>
+        /// Get the gameobject coupled to the interaction
+        /// </summary>
+        /// <param name="invoker">The gameobject that wants to get the other gameobject</param>
+        public virtual GameObject GameObject(GameObject invoker = null)
+        {
+            throw new NotImplementedException();
         }
     }
 }
